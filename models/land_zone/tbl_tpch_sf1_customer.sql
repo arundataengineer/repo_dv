e67aea65b6fbaba541d71dae37f29be1ca4d394f
Arun Raja Anbalagan <arunrajaazure1@gmail.com>
2025-05-02 08:49:53 +0000

    Commiting the customer table

---
+++ models/land_zone/tbl_tpch_sf1_customer.sql
@@ -0,0 +1,26 @@
+{{
+    config( 
+            materialized = 'table',
+            database = var('dev_db'),
+            schema = var('dev_land_schema'),
+            alias = 'TPCH_SF1_CUSTOMER'
+    )
+}}
+
+WITH CUST AS
+(
+select
+    C_CUSTKEY, 
+    C_NAME, 
+    C_ADDRESS, 
+    C_NATIONKEY, 
+    C_PHONE, 
+    C_ACCTBAL, 
+    C_MKTSEGMENT, 
+    C_COMMENT,
+    current_timestamp() INTS ,
+    current_date() as indt,
+    '{{var("rec_src")}}' AS Record_source
+from {{source('sf_tpch','CUSTOMER')}}
+)
+SELECT * FROM CUST
